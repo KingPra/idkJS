4294144f1d259393552aee7b7ee22c678b55b6da
KingPra <kingi.pra@gmail.com>
2016-12-09 21:14:00 -0500

    work done friday

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ console.log('SUM');
 function sum(a, b) {
     return sum = a + b;
 }
-sum( 5, 5);
+sum(5, 5);
 
 console.log(sum);
 
@@ -22,10 +22,8 @@ console.log('AVG');
 function avg(num) {
     let sum = 0;
     for (let c = 0; c < num.length; c++) {
-         sum = sum + num[c];
-
+        sum = sum + num[c];
     }
-
     return sum / num.length;
 }
 let average = avg([3, 5, 4]);
@@ -40,12 +38,12 @@ console.log('GREATER THAN');
 
 function greaterThan(a, b) {
     if (a < b) {
-    return "true"
-   
- }else{
+        return "true"
 
-     return false;
- }
+    } else {
+
+        return false;
+    }
 }
 
 let ans = greaterThan(5, 4)
@@ -57,6 +55,21 @@ console.log(ans);
 //-------------------#4. secondLargest ---------
 
 
+let height = [20, 50, 100, 30, 75, 24, 99];
+tallestMtn = 0;
+for (i = 0; i < height.length; i++) {
+    console.log([i]);
+
+    // if hegiht of current item is bigger than height of tallest seen
+    if (tallestMtn < height[i]) {
+        tallestMtn = height[i];
+    }
+
+}
+console.log(tallestMtn);
+//console.log('tallestMtn') 
+
+
 // 1. loop through array.
 // 2. compare each value in the array to find the largest array. 
 // 3. store that value in a variable.
@@ -64,27 +77,58 @@ console.log(ans);
 // 5 compare the values stored in step 4.
 // print the smaller value.
 
-/*
+
 console.log('SECOND LARGEST');
-
-bigNum = 0;
-
+let arr = [1, 4, 8, 15, 2];
 function secondLargest(arr) {
+    let largest = 0;
+    // this is finding the largest number;
     for (let i = 0; i < arr.length; i++) {
+        if (largest < arr[i]) {
+            largest = arr[i];
+        } console.log(largest)
     }
-    
+    //this is ccopying over the largest
+    // largest = 0;
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (largest !== arr[i]) {
+            newArray.push(arr[i]);
+        
+        console.log(newArray);
+        }    
+}
+    let largest = 0;
+    for (let i = 0; i < newArray.length; i++) {
+        if (largest < newArray[i]) {
+            largest = newArray[i];
+        }
+    }
+
+    return largest;
 }
 
 
-secondLargest([3, 4, 5, 6])
+//console.log(secondLargest);
+//bigNum = 0;
+/*
+function secondLargest(arr) {
+    let sorted = arr.sort();
+    console.log(sorted);
+    return sorted[arr.length - 2];
 
+}
+let it = secondLargest(4, 3, 5, 6)
+console.log(it);
 */
+
+
 
 //---------#5. Contains Vowels-----------
 
-console.log('CONTAINS VOWELS');
+//console.log('CONTAINS VOWELS');
 
-
+/*
 // 1. variable containing Vowels
 // 2. 
 
@@ -101,14 +145,14 @@ function containsVowel(str) {
     
 }
 
-let word = containsVowel('wrd');
+let word = containsVowel('wide');
     console.log(word);
+/*
 
-
-
+/*
 //----------#6 PIG LATIN-------------
 
-console.log('PIG LATIN')
+console.log('PIG LATIN');
 
 function pigLatin(str) {
     let split = 0;
@@ -117,12 +161,12 @@ function pigLatin(str) {
      split = (str.split('')[0])
     }
     move = str.slice(1) + split + 'ay';
- console.log(move);
-
+    return move;
  //console.log(move);
 }
-pigLatin('iron');
-
+let pigL = pigLatin('yard');
+console.log(pigL);
+/*
 
 //-------------PRACTICE--------------
 
@@ -155,8 +199,129 @@ function fizzbuzz(max) {
             console.log('fizz');
         }else console.log(i);
 }
-}
+} .  /*
 
 fizzbuzz(21); //call the function, print out 1-21
+
+
+
+
+/**
+ * 07 | longestWord
+
+Write a function called longestWord that takes a single string and returns the longest word in the string.
+
+Hint: look into the split() function that you can call on strings.
+ */
+
+/*
+
+console.log('Longest Word');
+
+function longestWord(sentence) {
+    // 1. Convert sentence into an array
+    // 2. Search through the words for the largest letter count
+    // 3. once we've checked them all, return the longest one
+    let words = sentence.split(' '); //array of strings
+    let longest = 0; //the length of the longest word so far
+    let keeper = 0;
+    for (i = 0; i < words.length; i++) {
+       if (words[i].length > longest) {
+            longest = words[i].length; // we have a new longest word
+            keeper = words[i];
+            console.log('found new longest: ' + keeper);
+       }
+    }
+    //return a string
+    return keeper;
+}
+    let winner = longestWord( 'how much wood would a woodchuck chuck?');
+    console.log(winner);
+    
+*/
+
+// 1. sort through the string.
+// 2. count the letters in each word
+// 3. store the longest word.
+
+
+
+
+
+
+
+
+
+
+/**
+ * Problem #1: determine whether a string has more x's or o's.
+ * Input: stringReturn 'x' if more x's, or 'o'
+ if more 'o's 
+ */
+
+
+
+/**08 | divisors
+
+Write a function called divisors that accepts a number and returns an array of all of the numbers that divide evenly into it.
+
+ */
+/*
+console.log('DIVISORS');
+
+function divisors(num) {
+let divide = 0;
+divide
+}
+console.log(num);
+
+divisors(5);
+
+*/
+
+
+/**
+ * 09 | weave
+
+Write a function called weave() that accepts an input string and number. The function should return the string with every xth character replaced with an 'x'.
+ */
+
+
+
+
+
+
+
+/**
+ * Cherokee Hare
+
+There's a mythical species of rabbit called the Cherokee Hare that can reproduce at variable rates and never seem to die. Fortunately, some engineers figured out to write some code to describe it's future population. Wildlife experts rejoice! Create a variable called startingPopulation that keeps track of the initial population of hares, a variable called birthRate that keeps track of what percentage is born each week, and a variable called numberOfWeeks that represents how long we're trying to simulate. Print out "There will be x Cherokee Hares after y weeks" but replace x with the number of hares and y with the number of weeks.
+
+Note: it's common practice to represent rates (like the birth rate) as decimal values, i.e. 10% would be 0.1 instead of 10.
+
+For example, if:
+
+let startingPopulation = 200;
+let birthRate = 0.1;
+let numberOfWeeks = 5;
+
+... then you're trying to figure out how many hares will exist after five weeks if we start off with 200 and increase their population by 10% each week.
+ */
+
+
+//.1 create initial variables
+//.2 multiply population by birth rate.
+//.3 repeat step 2 per number of weeks.
+/*
+let startingPopulation = 200;
+let birthRate = 0.1;
+let numberOfWeeks = 5;
+let babies = 0;
+
+for(i = 0; i < numberOfWeeks; i++){
+ babies = startingPopulation * birthRate;
+startingPopulation = babies + startingPopulation;
+} 
+ console.log(Math.floor(startingPopulation));
 
 */
